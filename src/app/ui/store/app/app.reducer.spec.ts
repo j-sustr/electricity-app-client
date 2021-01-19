477b5f3e2245723fc60b18d06f5e357830c22eb6
Jan Sustr <jan.sustr11@gmail.com>
2021-01-19 20:09:44 +0100

    test: :white_check_mark: set interval action

---
+++ src/app/ui/store/app/app.reducer.spec.ts
@@ -0,0 +1,32 @@
+import { INITIAL_STATE } from '@ngrx/store';
+import { actionSetInterval } from './app.actions';
+import { appReducer, AppState, initialState } from './app.reducer';
+
+describe('AppReducer', () => {
+  const TEST_INTITIAL_STATE: AppState = {
+    intervals: [],
+  };
+
+  it('should return the default state', () => {
+    const action = {} as any;
+    const state = appReducer(undefined, action);
+
+    expect(state).toBe(initialState);
+  });
+
+  it('should set the interval', () => {
+    const action = actionSetInterval({
+      index: 0,
+      start: new Date(0),
+      end: new Date(1000),
+    });
+
+    const state = appReducer(TEST_INTITIAL_STATE, action);
+    expect(state.intervals).toEqual([
+      {
+        start: action.start,
+        end: action.end,
+      },
+    ]);
+  });
+});
