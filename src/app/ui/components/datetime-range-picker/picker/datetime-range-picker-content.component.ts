b0f319963e2d9481b8fafdb991b8b671d2a74fee
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-15 09:58:29 +0100

    feat(datetime-range-picker): target options

--- src/app/ui/components/datetime-range-picker/picker/datetime-range-picker-content.component.ts
+++ src/app/ui/components/datetime-range-picker/picker/datetime-range-picker-content.component.ts
@@ -1,24 +1,44 @@
 import { Component } from '@angular/core';
 import { Subject } from 'rxjs';
+import { DatetimeRangeSelectionModel } from '../input/datetime-range-selection-model';
 import { DatetimeRangePickerComponent } from './datetime-range-picker.component';
+
+export const targetOptions = ['year', 'month', 'day', 'hour'] as const;
+export type DatetimeRangePickerTarget = typeof targetOptions[number];
+
+export const stepOptions = ['select-target', 'select-value'] as const;
+export type DatetimeRangePickerStep = typeof stepOptions[number];
 
 @Component({
     selector: 'app-daterange-picker-content',
-    templateUrl: 'datetime-range-picker-content.component.html'
-    // providers: [CommonModule]
+    templateUrl: 'datetime-range-picker-content.component.html',
+    styleUrls: ['datetime-range-picker-content.component.scss'],
+    // eslint-disable-next-line @angular-eslint/no-host-metadata-property
+    host: {
+        class: 'datetime-range-picker-content'
+    }
 })
 export class DatetimeRangePickerContentComponent {
     picker?: DatetimeRangePickerComponent;
 
     _animationDone = new Subject<void>();
 
-    years = [2020, 2021, 2022];
+    step: DatetimeRangePickerStep = 'select-target';
 
-    constructor() {}
+    target: DatetimeRangePickerTarget | null = null;
 
-    _handleUserSelection(event: unknown) {
-        throw Error('not implemented');
+    constructor(private _model: DatetimeRangeSelectionModel) {}
+
+    selectTarget(value: DatetimeRangePickerTarget): void {
+        this.target = value;
+        this.step = 'select-value';
     }
+
+    selectYear() {}
+
+    selectMonth() {}
+
+    selectDay() {}
 
     _startExitAnimation() {
         throw Error('not implemented');
