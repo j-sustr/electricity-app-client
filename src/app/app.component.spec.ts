2dd463dbeaf56ccd96e1eae20157b3303937f7ba
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-16 16:01:18 +0100

    cleanup

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,10 +15,4 @@ describe('AppComponent', () => {
         const app = fixture.componentInstance;
         expect(app).toBeTruthy();
     });
-
-    it(`should have as title 'angular-electricity-app'`, () => {
-        const fixture = TestBed.createComponent(AppComponent);
-        const app = fixture.componentInstance;
-        expect(app.title).toEqual('angular-electricity-app');
-    });
 });
