87f1cfde7fb488b7033ec81ac93343f586d49ab7
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-18 22:25:24 +0100

    feat: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+        path: 'power-factor',
+        loadChildren: () =>
+            import('./ui/pages/power-factor/power-factor.module').then(
+                (m) => m.PowerFactorModule
+            )
+    },
+    {
+        path: '**',
+        redirectTo: 'costs'
+    }
+];
 
 @NgModule({
     imports: [RouterModule.forRoot(routes)],
