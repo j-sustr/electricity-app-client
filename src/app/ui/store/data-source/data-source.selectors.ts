c8532ccbed2662a7f820123fbe94050e1432c029
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-21 14:30:38 +0100

    feat: impl selectors

---
+++ src/app/ui/store/data-source/data-source.selectors.ts
@@ -0,0 +1,12 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { AppState } from '../app-store.state';
+import { DataSourceState } from './data-source.reducer';
+
+const selectDataSource = createFeatureSelector<AppState, DataSourceState>(
+    'dataSource'
+);
+
+export const selectDataSourceIntervals = createSelector(
+    selectDataSource,
+    (state: DataSourceState) => state.intervals
+);
