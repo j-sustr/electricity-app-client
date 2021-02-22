3ed19e58a55e20a53b9e3b392faf4dba7d1eb167
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-02-22 18:08:55 +0100

    feat: impl table

--- src/app/ui/pages/power-factor/overview/table/power-factor-overview-table.component.ts
+++ src/app/ui/pages/power-factor/overview/table/power-factor-overview-table.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/internal/operators/map';
+import { AppState } from 'src/app/ui/store/app-store.state';
+import { selectPFOverviewViewItems } from 'src/app/ui/store/power-factor-overview/power-factor-overview.selectors';
+import { PowerFactorOverviewItem } from 'src/app/web-api-client';
 
 @Component({
-  selector: 'app-power-factor-overview-table',
-  templateUrl: './power-factor-overview-table.component.html',
-  styleUrls: ['./power-factor-overview-table.component.scss']
+    selector: 'app-power-factor-overview-table',
+    templateUrl: './power-factor-overview-table.component.html',
+    styleUrls: ['./power-factor-overview-table.component.scss']
 })
-export class PowerFactorOverviewTableComponent implements OnInit {
+export class PowerFactorOverviewTableComponent {
+    items$: Observable<PowerFactorOverviewItem[]>;
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+    constructor(private store: Store<AppState>) {
+        this.items$ = this.store.pipe(
+            select(selectPFOverviewViewItems),
+            map((items) => {
+                return items ?? [];
+            })
+        );
+    }
 }
--- src/app/ui/pages/power-factor/power-factor.module.ts
+++ src/app/ui/pages/power-factor/power-factor.module.ts
@@ -7,10 +7,10 @@ import {
 } from 'devextreme-angular';
 import { DatetimeRangePickerModule } from '../../components/datetime-range-picker/datetime-range-picker.module';
 import { PowerFactorDetailComponent } from './detail/power-factor-detail.component';
+import { PowerFactorOverviewChartComponent } from './overview/chart/power-factor-overview-chart.component';
 import { PowerFactorOverviewComponent } from './overview/power-factor-overview.component';
-import { PowerFactorRoutingModule } from './power-factor-routing.module';
 import { PowerFactorOverviewTableComponent } from './overview/table/power-factor-overview-table.component';
-import { PowerFactorOverviewChartComponent } from './overview/table/power-factor-overview-chart.component';
+import { PowerFactorRoutingModule } from './power-factor-routing.module';
 
 const DX = [DxDataGridModule, DxTemplateModule, DxBulletModule];
 
