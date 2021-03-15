38f09c1dcb0d51a6763f422b534bf4e29f63d14a
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-15 10:01:38 +0100

    feat: pf overview chart click navigation

--- src/app/ui/pages/power-factor/overview/chart/power-factor-overview-chart.component.ts
+++ src/app/ui/pages/power-factor/overview/chart/power-factor-overview-chart.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { AppState } from 'src/app/ui/store/app-store.state';
+import { dataSourceReducer } from 'src/app/ui/store/data-source/data-source.reducer';
 import {
     PowerFactorOverviewChart,
     selectOverviewChart
@@ -15,7 +17,7 @@ import {
 export class PowerFactorOverviewChartComponent {
     chart$: Observable<PowerFactorOverviewChart | null>;
 
-    constructor(private store: Store<AppState>) {
+    constructor(private store: Store<AppState>, private router: Router) {
         this.chart$ = this.store.pipe(select(selectOverviewChart));
     }
 
@@ -38,4 +40,14 @@ export class PowerFactorOverviewChartComponent {
         }
         return text + 'Wh';
     };
+
+    async handleClick(event: {
+        target?: { data?: { groupId?: string } };
+    }): Promise<void> {
+        console.log(event);
+        const groupId = event.target?.data?.groupId;
+        if (groupId) {
+            await this.router.navigate(['power-factor', 'detail', groupId]);
+        }
+    }
 }
