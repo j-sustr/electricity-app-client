f45c4661e56a4953da5a339b77ec96ad4b227ea1
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-03-29 23:34:29 +0200

    reimplem login form as reactive form

--- src/app/ui/pages/login/login-form/login-form.component.ts
+++ src/app/ui/pages/login/login-form/login-form.component.ts
@@ -1,8 +1,15 @@
+/* eslint-disable @typescript-eslint/unbound-method */
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { AppState } from 'src/app/store/app-store.state';
 import { login } from 'src/app/store/auth/auth.actions';
+import { openDataSource } from 'src/app/store/data-source/data-source.actions';
+import {
+    DataSourceType,
+    DBConnectionParams,
+    TenantDto
+} from 'src/app/web-api-client';
 
 interface FormData {
     email?: string;
@@ -15,13 +22,63 @@ interface FormData {
     styleUrls: ['./login-form.component.scss']
 })
 export class LoginFormComponent {
+    dataSourceTypes = Object.freeze(['Database', 'CEA File']) as string[];
+
     loading = false;
     formData: FormData = {};
 
-    constructor(
-        private store: Store<AppState>,
-        private router: Router // private notificationsService: NotificationService
-    ) {}
+    form = this.fb.group({
+        dataSourceType: [null, [Validators.required]],
+        dbConnectionParams: this.fb.group({
+            server: [null, Validators.required],
+            dbName: [null, Validators.required],
+            username: [null, Validators.required],
+            password: [null, Validators.required]
+        }),
+        ceaFilename: [null, [Validators.required]],
+        server: [null, [Validators.required]],
+        username: [null, [Validators.required]],
+        password: [null, [Validators.required]]
+    });
+
+    dataSourceTypeControl = this.form.get('dataSourceType');
+    dbConnectionServerControl = this.form.get('dbConnectionParams.server');
+    dbConnectionDBNameControl = this.form.get('dbConnectionParams.dbName');
+    dbConnectionUsernameControl = this.form.get('dbConnectionParams.username');
+    dbConnectionPasswordControl = this.form.get('dbConnectionParams.password');
+    ceaFilenameControl = this.form.get('ceaFilename');
+    serverControl = this.form.get('server');
+    usernameControl = this.form.get('username');
+    passwordControl = this.form.get('password');
+
+    constructor(private store: Store<AppState>, private fb: FormBuilder) {}
+
+    open(): void {
+        this.store.dispatch(
+            openDataSource({
+                tenant: new TenantDto({
+                    dataSourceType: this.form.get('dataSourceType')
+                        ?.value as DataSourceType,
+                    dbConnectionParams: this.form.get('dbConnectionParams')
+                        ?.value as DBConnectionParams,
+                    ceaFileName: this.form.get('ceaFilename')?.value as string
+                })
+            })
+        );
+
+        login({
+            username: this.form.get('username')?.value as string,
+            password: this.form.get('password')?.value as string
+        });
+    }
+
+    submit(): void {
+        this.form.markAllAsTouched();
+        if (!this.form.valid) {
+            return;
+        }
+        this.open();
+    }
 
     onSubmit(e: Event): void {
         e.preventDefault();
--- src/app/ui/pages/login/login.module.ts
+++ src/app/ui/pages/login/login.module.ts
@@ -1,21 +1,24 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { DxFormModule } from 'devextreme-angular';
+import { ReactiveFormsModule } from '@angular/forms';
 import { DxTemplateModule } from 'devextreme-angular/core';
 import { DxButtonModule } from 'devextreme-angular/ui/button';
 import { DxLoadIndicatorModule } from 'devextreme-angular/ui/load-indicator';
+import { DxRadioGroupModule } from 'devextreme-angular/ui/radio-group';
+import { DxTextBoxModule } from 'devextreme-angular/ui/text-box';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { LoginRoutingModule } from './login-routing.module';
 
 const DX = [
-    DxFormModule,
     DxTemplateModule,
+    DxTextBoxModule,
+    DxRadioGroupModule,
     DxLoadIndicatorModule,
     DxButtonModule
 ];
 
 @NgModule({
-    imports: [CommonModule, ...DX, LoginRoutingModule],
+    imports: [CommonModule, ReactiveFormsModule, ...DX, LoginRoutingModule],
     declarations: [LoginFormComponent]
 })
 export class LoginModule {}
