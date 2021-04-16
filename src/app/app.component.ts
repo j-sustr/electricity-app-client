a086d3ab7a0fe16255567d0165c869908d1a579b
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-16 14:11:31 +0200

    impl devLogin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { select, Store } from '@ngrx/store';
+import { AfterViewInit, Component } from '@angular/core';
+import { ActionsSubject, select, Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 import { AppState } from './app/app-store.state';
 import { selectIsAuthenticated } from './app/auth/auth.selectors';
 import { AuthService } from './app/auth/auth.service';
@@ -15,20 +16,20 @@ import {
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
     title = 'angular-electricity-app';
 
     isAuthenticated$: Observable<boolean>;
 
     constructor(
         private store: Store<AppState>,
+        private actionsSubject: ActionsSubject,
         private authService: AuthService,
         private groupsClient: GroupsClient,
         private dataSourceClient: DataSourceClient,
         private dbDataSourceClient: DBDataSourceClient
     ) {
         this.isAuthenticated$ = this.store.pipe(select(selectIsAuthenticated));
-
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         (window as any).authService = authService;
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
@@ -37,5 +38,13 @@ export class AppComponent {
         (window as any).dataSourceClient = dataSourceClient;
         // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
         (window as any).dbDataSourceClient = dbDataSourceClient;
+    }
+
+    ngAfterViewInit(): void {
+        if (!environment.production) {
+            void import('./dev-login').then((module) => {
+                module.devLogin(this.store, this.actionsSubject);
+            });
+        }
     }
 }
