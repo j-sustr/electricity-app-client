5167b98491695f2f07cd062e0698f0b5c4244b7b
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-20 17:18:21 +0200

    generate api

--- src/app/web-api-client.ts
+++ src/app/web-api-client.ts
@@ -401,6 +401,7 @@ export interface IDataSourceClient {
     getTenant(): Observable<FileResponse>;
     setTenant(identifier: string | null | undefined): Observable<FileResponse>;
     open(command: OpenDataSourceCommand): Observable<DataSourceInfoDto>;
+    getInfo(): Observable<DataSourceInfoDto>;
     clearCache(): Observable<FileResponse>;
 }
 
@@ -542,6 +543,54 @@ export class DataSourceClient implements IDataSourceClient {
     }
 
     protected processOpen(response: HttpResponseBase): Observable<DataSourceInfoDto> {
+        const status = response.status;
+        const responseBlob =
+            response instanceof HttpResponse ? response.body :
+            (<any>response).error instanceof Blob ? (<any>response).error : undefined;
+
+        let _headers: any = {}; if (response.headers) { for (let key of response.headers.keys()) { _headers[key] = response.headers.get(key); }}
+        if (status === 200) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            let result200: any = null;
+            let resultData200 = _responseText === "" ? null : JSON.parse(_responseText, this.jsonParseReviver);
+            result200 = DataSourceInfoDto.fromJS(resultData200);
+            return _observableOf(result200);
+            }));
+        } else if (status !== 200 && status !== 204) {
+            return blobToText(responseBlob).pipe(_observableMergeMap(_responseText => {
+            return throwException("An unexpected server error occurred.", status, _responseText, _headers);
+            }));
+        }
+        return _observableOf<DataSourceInfoDto>(<any>null);
+    }
+
+    getInfo(): Observable<DataSourceInfoDto> {
+        let url_ = this.baseUrl + "/api/DataSource/info";
+        url_ = url_.replace(/[?&]$/, "");
+
+        let options_ : any = {
+            observe: "response",
+            responseType: "blob",
+            headers: new HttpHeaders({
+                "Accept": "application/json"
+            })
+        };
+
+        return this.http.request("get", url_, options_).pipe(_observableMergeMap((response_ : any) => {
+            return this.processGetInfo(response_);
+        })).pipe(_observableCatch((response_: any) => {
+            if (response_ instanceof HttpResponseBase) {
+                try {
+                    return this.processGetInfo(<any>response_);
+                } catch (e) {
+                    return <Observable<DataSourceInfoDto>><any>_observableThrow(e);
+                }
+            } else
+                return <Observable<DataSourceInfoDto>><any>_observableThrow(response_);
+        }));
+    }
+
+    protected processGetInfo(response: HttpResponseBase): Observable<DataSourceInfoDto> {
         const status = response.status;
         const responseBlob =
             response instanceof HttpResponse ? response.body :
