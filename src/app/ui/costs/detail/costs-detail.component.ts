3f76f54d2b9492dc0132f8219edd35a5e33bd3f6
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-04-29 23:23:17 +0200

    add customer params form prompt to costs detail

--- src/app/ui/costs/detail/costs-detail.component.ts
+++ src/app/ui/costs/detail/costs-detail.component.ts
@@ -4,6 +4,8 @@ import { Observable } from 'rxjs';
 import { AppState } from 'src/app/app/app-store.state';
 import { CostsDetailState } from 'src/app/app/costs-detail/costs-detail.model';
 import { selectDetail } from 'src/app/app/costs-detail/costs-detail.selectors';
+import { setIsCustomerParamsPopupFormOpen } from 'src/app/app/costs/costs.actions';
+import { selectHasCustomerParams } from 'src/app/app/costs/costs.selectors';
 
 @Component({
     selector: 'app-costs-detail',
@@ -11,9 +13,21 @@ import { selectDetail } from 'src/app/app/costs-detail/costs-detail.selectors';
     styleUrls: ['./costs-detail.component.scss']
 })
 export class CostsDetailComponent {
+    hasCustomerParams$: Observable<boolean>;
     state$: Observable<CostsDetailState>;
 
     constructor(private store: Store<AppState>) {
         this.state$ = this.store.pipe(select(selectDetail));
+        this.hasCustomerParams$ = this.store.pipe(
+            select(selectHasCustomerParams)
+        );
+    }
+
+    handleCustomerParamsBtnClick(): void {
+        this.store.dispatch(
+            setIsCustomerParamsPopupFormOpen({
+                open: true
+            })
+        );
     }
 }
