512ef4dd7aba424e94c3ad6f66360aa81386e88e
Jan Sustr <25587140+jansustr11@users.noreply.github.com>
2021-05-16 17:38:59 +0200

    install formatjs polyfills

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import '@formatjs/intl-getcanonicallocales/polyfill';
+import '@formatjs/intl-locale/polyfill';
+import '@formatjs/intl-datetimeformat/polyfill';
+import '@formatjs/intl-datetimeformat/locale-data/en'; // locale-data for en
+import '@formatjs/intl-datetimeformat/add-all-tz'; // Add ALL tz data
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
